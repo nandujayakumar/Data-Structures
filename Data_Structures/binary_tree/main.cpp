06643d5f21cd68637174d774e66f64cf227c6f92
Nandu_Velikad <nanduvelikad007@gmail.com>
2019-04-23 17:58:57 +0530

    binary tree height function removed

--- Data_Structures/binary_tree/main.cpp
+++ Data_Structures/binary_tree/main.cpp
@@ -6,20 +6,13 @@ using namespace std;
 int main ()
 {
 	binaryTree obj;
-	obj.insert_element(50);
-	obj.insert_element(45);
-	obj.insert_element(55);
-	obj.insert_element(30);
-	obj.insert_element(47);
-	obj.insert_element(57);
-	obj.insert_element(53);
-	obj.insert_element(20);
-	obj.insert_element(35);
-	obj.insert_element(56);
-	obj.insert_element(58);
-	obj.insert_element(36);
-	obj.delete_element (35);
-	obj.levelorder_traversal();
+	obj.insert_element(3);
+	obj.insert_element(5);
+	obj.insert_element(2);
+	obj.insert_element(1);
+	obj.insert_element(4);
+	obj.insert_element(6);
+	obj.insert_element(7);
     cout << endl;
     cout << obj.find_height() << endl;
 	
